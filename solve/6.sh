2d3fe27fc7d02150ff350fe60da48aa6851ec49c
Ali-Akber Saifee <ali@indydevs.org>
2022-12-06 05:52:47 -0800

    Day 6

---
+++ solve/6.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+solve() {
+	declare -a buffer;
+	for char in $(cat $1 | fold -w1); do
+		buffer+=($char)
+		part=(${buffer[@]: -$2})
+		if [[ "${#part[@]}" = $2 ]]; then
+			uniq=($(echo ${part[@]} | tr -d ' ' | fold -w1 | sort | uniq))
+			if [[ "${#uniq[@]}" = $2 ]]; then
+				found=(${buffer[@]/${part[@]}})
+				echo ${#found[@]}
+				break
+			fi
+
+		fi
+	done
+
+}
+
+echo part 1: "$(solve $1 4)"
+echo part 2: "$(solve $1 14)"
