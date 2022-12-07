58ae409d85ee4d1c13a7e51b49fcda18ee9f70bb
Ali-Akber Saifee <ali@indydevs.org>
2022-12-07 06:57:47 -0800

    Day 7, Part 2

--- solve/7.sh
+++ solve/7.sh
@@ -41,4 +41,58 @@ part1() {
 	echo $total
 }
 
+part2() {
+	max=$2
+	required=$3
+	declare -a dirstack
+	declare -A sizes
+	mode=command
+	while read line; do
+		if [[ "$line" =~ ^\$[[:space:]](cd|ls)([[:space:]](.*))* ]]; then
+			mode="cmd"
+			cmd=${BASH_REMATCH[1]}
+			arg=${BASH_REMATCH[3]}
+			if [[ "$cmd" = "cd" ]]; then
+				if [[ $arg = ".." ]]; then
+					child=${dirstack[-1]}
+					unset dirstack[-1]
+					((sizes[${dirstack[-1]}] += ${sizes[$child]}))
+				else
+					if ((${#dirstack[@]} > 1)); then
+						dirstack+=("${dirstack[-1]}/$arg")
+					else
+						dirstack+=($arg)
+					fi
+				fi
+			elif [[ "$cmd" = "ls" ]]; then
+				mode="list"
+			fi
+		else
+			if [[ ! "$line" =~ ^dir.* ]]; then
+				size=${line% *}
+				cur=${dirstack[-1]}
+				((sizes[$cur] += $size))
+			fi
+		fi
+	done <$1
+	while (( ${#dirstack[@]} > 1 )); do
+		child=${dirstack[-1]}
+		unset dirstack[-1]
+		(( sizes[${dirstack[-1]}]+=${sizes[$child]} ))
+	done
+
+	left=$((($max-${sizes[/]})))
+	reclaim=$((($required-$left)))
+	candidate=""
+	for dir in ${!sizes[@]}; do
+		if (( ${sizes[$dir]} >= $reclaim )); then
+			if [[ -z $candidate ]] || (( ${sizes[$dir]} < ${sizes[$candidate]} )); then
+				candidate=$dir
+			fi
+		fi
+	done
+	echo ${sizes[$candidate]}
+}
+
 echo part 1: "$(part1 $1)"
+echo part 2: "$(part2 $1 70000000 30000000)"
