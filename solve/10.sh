38efc2ba07ce1d227c97264ec66504a17c737374
Ali-Akber Saifee <ali@indydevs.org>
2022-12-10 11:54:55 -0800

    Day 10, part 2, silly

--- solve/10.sh
+++ solve/10.sh
@@ -25,41 +25,18 @@ part1() {
 }
 
 part2() {
-	OIFS="$IFS"
-	declare -a values=(1)
-	while read -r instruction; do
-		set -- $instruction
-		cmd=$1
-		arg=$2
-		cur=${values[-1]}
-		if [[ "$cmd" = "noop" ]]; then
-			values+=(${values[-1]})
-		else
-			values+=(${values[-1]})
-			values+=($(((${values[-1]} + arg))))
-		fi
-
-	done <$1
-	sum=0
-	for cycle in "1,40" "41,80" "81,120" "121,160" "161,200" "201,240"; do
-		IFS=,
-		set -- $cycle
-		range=$(eval echo "{$1..$2}")
-		IFS="$OIFS"
-		for c in $range; do
-			pos=${values[$((c - 1))]}
-			active=$((pos - ((c - $1))))
-			active=$((${active#-} <= 1))
-			if [[ $active = 1 ]]; then
-				echo >&2 -n '#'
-			else
-				echo >&2 -n '.'
-			fi
-		done
-		echo >&2
-	done
+	echo
+	cat $1 |
+		sed -e 's/noop/0/' -Ee 's/addx (.)/0\n\1/' |
+		sed -Ee 's/^([[:digit:]]+)/p\n\1+/' -Ee 's/^-([[:digit:]]+)/p\n\1-/' |
+		dc -e1 -f - |
+		nl -v0 -w2 -s' ' |
+		sed -E -e 's/([[:digit:]]+) (-*[[:digit:]]+)/((\1%40-(\2))^2 <= 1)/' |
+		bc |
+		tr '0' '.' | tr '1' '#' |
+		paste -sd'\0' |
+		fold -w40
 
 }
-
 echo part 1: $(part1 $1)
-echo part 2: $(part2 $1)
+echo part 2: "$(part2 $1)"
