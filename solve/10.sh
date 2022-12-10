4cef22681a91e4455f65ef939f6732597285dae2
Ali-Akber Saifee <ali@indydevs.org>
2022-12-10 14:21:35 -0800

    Day 10, part 1, silly

--- solve/10.sh
+++ solve/10.sh
@@ -1,33 +1,22 @@
 #!/usr/bin/env bash
 
 part1() {
-	OIFS="$IFS"
-	declare -a values=(1)
-	while read -r instruction; do
-		set -- $instruction
-		cmd=$1
-		arg=$2
-		cur=${values[-1]}
-		if [[ "$cmd" = "noop" ]]; then
-			values+=(${values[-1]})
-		else
-			values+=(${values[-1]})
-			values+=($(((${values[-1]} + arg))))
-		fi
-
-	done <$1
-	sum=0
-	for cycle in 20 60 100 140 180 220; do
-		((sum += cycle * ${values[cycle - 1]}))
-	done
-	echo $sum
-
+	echo
+	cat $1 |
+		sed -e 's/noop/0/' -Ee 's/addx (.*)/0\n\1/' |
+		sed -Ee 's/^([[:digit:]]+)/p\n\1+/' -Ee 's/^-([[:digit:]]+)/p\n\1-/' |
+		dc -e1 -f - |
+		nl -w2 -s' ' |
+		grep -E "^(20|60|100|140|180|220) " |
+		sed -E -e 's/([[:digit:]]+) (-*[[:digit:]]+)/\1*\2/' |
+		paste -sd+ |
+		bc
 }
 
 part2() {
 	echo
 	cat $1 |
-		sed -e 's/noop/0/' -Ee 's/addx (.)/0\n\1/' |
+		sed -e 's/noop/0/' -Ee 's/addx (.*)/0\n\1/' |
 		sed -Ee 's/^([[:digit:]]+)/p\n\1+/' -Ee 's/^-([[:digit:]]+)/p\n\1-/' |
 		dc -e1 -f - |
 		nl -v0 -w2 -s' ' |
@@ -38,5 +27,5 @@ part2() {
 		fold -w40
 
 }
-echo part 1: $(part1 $1)
+echo part 1: "$(part1 $1)"
 echo part 2: "$(part2 $1)"
